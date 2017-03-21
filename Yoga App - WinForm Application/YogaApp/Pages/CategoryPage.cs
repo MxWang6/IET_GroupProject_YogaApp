4fcc17d203eba05e15f2c15df1c23bd2e1fd63fb
Saurabh Pathak <pathaks@tcd.ie>
2017-03-21 11:18:21 +0000

    pushing excel reader

--- Yoga App - WinForm Application/YogaApp/Pages/CategoryPage.cs
+++ Yoga App - WinForm Application/YogaApp/Pages/CategoryPage.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
+using YogaApp.Utils;
 
 namespace YogaApp
 {
@@ -18,6 +12,7 @@ namespace YogaApp
         {
             InitializeComponent();
             categoryList = new CategoryList(this);
+            CsvReader.getExcelFile();
         }
 
         private void navigateToCategoryListing()
