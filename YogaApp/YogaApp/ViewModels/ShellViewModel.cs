ee326ec41bf7adea0df94f00ffac9a48b9b8b720
maxim1um <maximluo@gmail.com>
2017-03-21 02:12:24 +0000

    Add Setting page and About page

--- YogaApp/YogaApp/ViewModels/ShellViewModel.cs
+++ YogaApp/YogaApp/ViewModels/ShellViewModel.cs
@@ -19,8 +19,11 @@ namespace YogaApp.ViewModels
             //this.Menu.Add(new MenuItem() { Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.Coffee }, Text = "Break", NavigationDestination = new Uri("Views/BreakPage.xaml", UriKind.RelativeOrAbsolute) });
             //this.Menu.Add(new MenuItem() { Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.FontAwesome }, Text = "Awesome", NavigationDestination = new Uri("Views/AwesomePage.xaml", UriKind.RelativeOrAbsolute) });
 
-            this.OptionsMenu.Add(new MenuItem() { Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.Cogs }, Text = "Settings", NavigationDestination = new Uri("Views/SettingsPage.xaml", UriKind.RelativeOrAbsolute) });
-            this.OptionsMenu.Add(new MenuItem() { Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.InfoCircle }, Text = "About", NavigationDestination = new Uri("Views/AboutPage.xaml", UriKind.RelativeOrAbsolute) });
+            //this.OptionsMenu.Add(new MenuItem() { Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.Cogs }, Text = "Settings", NavigationDestination = new Uri("Views/SettingsPage.xaml", UriKind.RelativeOrAbsolute) });
+            //this.OptionsMenu.Add(new MenuItem() { Icon = new PackIconFontAwesome() { Kind = PackIconFontAwesomeKind.InfoCircle }, Text = "About", NavigationDestination = new Uri("Views/AboutPage.xaml", UriKind.RelativeOrAbsolute) });
+
+            this.OptionsMenu.Add(new MenuItem() {Icon = new PackIconModern() {Kind = PackIconModernKind.Settings}, Text = "Settings", NavigationDestination = new Uri("Views/Settings.xaml", UriKind.RelativeOrAbsolute) });
+            this.OptionsMenu.Add(new MenuItem() {Icon = new PackIconModern() {Kind = PackIconModernKind.Information}, Text = "About", NavigationDestination = new Uri("Views/About.xaml", UriKind.RelativeOrAbsolute) });
         }
     }
 }
---
+++ YogaApp/YogaApp/Views/Settings.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using System.Windows.Navigation;
+
+namespace YogaApp.Views
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : Page
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private async void Quit_Click(object sender, RoutedEventArgs e)
+        {
+            var window = TreeHelper.TryFindParent<MainWindow>(this);
+            var mySettings = new MetroDialogSettings()
+            {
+                AffirmativeButtonText = "Quit",
+                NegativeButtonText = "Cancel",
+                AnimateShow = true,
+                AnimateHide = true
+            };
+
+            var result = await DialogManager.ShowMessageAsync(window, "Quit application?",
+                "Sure you want to quit application?", MessageDialogStyle.AffirmativeAndNegative, mySettings);
+            var shutdown = result == MessageDialogResult.Affirmative;
+            if (shutdown)
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
