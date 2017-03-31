72aec4d839263e0a6134fb12220b3338b870b4a5
Eduardo Munoz Martinez <munozmae@tcd.ie>
2017-03-31 13:08:34 +0100

    kinect program merged, evaluate the position for 4 seconds and then close

--- KinectExplorer-D2D/stdafx.h
+++ KinectExplorer-D2D/stdafx.h
@@ -1,4 +1,4 @@
-﻿//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // <copyright file="stdafx.h" company="Microsoft">
 //     Copyright (c) Microsoft Corporation.  All rights reserved.
 // </copyright>
@@ -12,13 +12,22 @@
 
 // Windows Header Files
 #include <windows.h>
+#include <ShellAPI.h>
 
 #include <Shlobj.h>
 
-// Direct2D Header Files
-#include <d2d1.h>
+// Assert Header Files
+#include <assert.h>
 
-#pragma comment ( lib, "d2d1.lib" )
+
+// The user defined message
+#define WM_UPDATEMAINWINDOW             WM_USER + 1
+#define WM_CLOSEKINECTWINDOW            WM_USER + 2
+#define WM_STREAMEVENT                  WM_USER + 3
+#define WM_TIMEREVENT                   WM_USER + 4
+#define WM_SHOWKINECTWINDOW             WM_USER + 5
+
+static const UINT MaxStringChars = 256;
 
 #ifdef _UNICODE
 #if defined _M_IX86
@@ -29,14 +38,3 @@
 #pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #endif
 #endif
-
-// Safe release for interfaces
-template<class Interface>
-inline void SafeRelease( Interface *& pInterfaceToRelease )
-{
-    if ( pInterfaceToRelease != NULL )
-    {
-        pInterfaceToRelease->Release();
-        pInterfaceToRelease = NULL;
-    }
-}
